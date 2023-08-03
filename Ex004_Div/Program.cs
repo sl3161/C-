b6a842128493598e41fdcf319eb657364d28598c
«Zotov <sl3161@yandex.ru>
2023-08-03 11:56:50 +0300

    юю

--- Ex004_Div/Program.cs
+++ Ex004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 17;
+double numberB = 5;
+Console.WriteLine ( numberA/numberB);
+
