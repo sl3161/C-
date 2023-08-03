b6c163f0989e5ffbbb16b63966d96a18293fab53
«Zotov <sl3161@yandex.ru>
2023-08-03 11:26:05 +0300

    Hello User

---
+++ Ex002_Hello_User/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write ("Привет,");
+Console.WriteLine (username);
+
+
+
