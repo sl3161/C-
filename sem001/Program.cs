67194ab5ec7d57b03255ab7f978c89f3b1d0c3f9
«Zotov <sl3161@yandex.ru>
2023-08-07 18:20:14 +0300

    дни недели с return

--- sem001/Program.cs
+++ sem001/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.WriteLine ("Введите число 1");
+﻿/*Console.WriteLine ("Введите число 1");
 int num1 = int.Parse (Console.ReadLine ());
 Console.WriteLine ("Введите число 2");
 int num2 = int.Parse (Console.ReadLine ());
-/*if (num1==num2*num2)
+if (num1==num2*num2)
 {
     Console.WriteLine (" Число 1 это квадрат числа 2");
 }
@@ -10,5 +10,47 @@ else
 {
     Console.WriteLine (" Число 1 это не квадрат числа 2");
 }*/
-Console.WriteLine ( num1==num2*num2 ? "Число 1 является квадратом числа 2" : "Число 1 не является квадратом числа 2");
+/*Console.WriteLine ( num1==num2*num2 ? "Число 1 является квадратом числа 2" : "Число 1 не является квадратом числ)*/
+
+Console.WriteLine ("Введите номер дня ");
+int day = int.Parse(Console.ReadLine ());
+if (day==1)
+{
+    Console.WriteLine ("Понедельник");
+    return;
+}
+if (day==2)
+{
+    Console.WriteLine ("Вторник");
+    return;
+}
+if (day==1)
+{
+    Console.WriteLine ("Среда");
+    return;
+}
+
+if (day==4)
+{
+    Console.WriteLine ("Четверг");
+    return;
+}
+if (day==5)
+{
+    Console.WriteLine ("Пятница");
+    return;
+}
+if (day==6)
+{
+    Console.WriteLine ("Суббота");
+    return;
+}
+if (day==7)
+{
+   Console.WriteLine ("Воскресенье");
+   return;
+}
+
+
+
 
