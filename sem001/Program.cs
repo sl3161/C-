a8b02eeafda99b691bf69185642ff26ee8085b58
«Zotov <sl3161@yandex.ru>
2023-08-07 17:52:24 +0300

    Вторая задача на сравнение с квадратом второго числа- второй способ

--- sem001/Program.cs
+++ sem001/Program.cs
@@ -2,12 +2,13 @@
 int num1 = int.Parse (Console.ReadLine ());
 Console.WriteLine ("Введите число 2");
 int num2 = int.Parse (Console.ReadLine ());
-if (num1==num2*num2)
+/*if (num1==num2*num2)
 {
     Console.WriteLine (" Число 1 это квадрат числа 2");
 }
 else
 {
     Console.WriteLine (" Число 1 это не квадрат числа 2");
-}
+}*/
+Console.WriteLine ( num1==num2*num2 ? "Число 1 является квадратом числа 2" : "Число 1 не является квадратом числа 2");
 
