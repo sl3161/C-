56e05f53207294ee548907b56564d325506ab17a
«Zotov <sl3161@yandex.ru>
2023-08-07 17:37:48 +0300

    Вторая задача на сравнение с квадратом второго числа

--- sem001/Program.cs
+++ sem001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите число 1");
+int num1 = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Введите число 2");
+int num2 = int.Parse (Console.ReadLine ());
+if (num1==num2*num2)
+{
+    Console.WriteLine (" Число 1 это квадрат числа 2");
+}
+else
+{
+    Console.WriteLine (" Число 1 это не квадрат числа 2");
+}
+
