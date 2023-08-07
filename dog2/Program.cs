993a88cfe3f007ab2b772db924fad97b543302a4
«Zotov <sl3161@yandex.ru>
2023-08-07 11:26:18 +0300

    ввод данных

---
+++ dog2/Program.cs
@@ -0,0 +1,9 @@
+﻿int count = 0 ;
+Console.WriteLine("введите расстояние между людьми");
+double distance = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("введите скорость 1-го человека");
+double v1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("введите скорость 2-го человека");
+double v2 = Convert.ToDouble(Console.ReadLine ());
+Console.WriteLine ("введите скорость собаки");
+double v_dog = Convert.ToDouble(Console.ReadLine ());
